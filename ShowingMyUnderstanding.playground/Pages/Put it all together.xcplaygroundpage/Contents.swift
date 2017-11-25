0aac8e6c63d2453ab1385af822c03654fb00c92f
Eric Xia <exia@lcs.on.ca>
2017-11-24 22:04:15 -0500

    Completed Putting it all Together

--- ShowingMyUnderstanding.playground/Pages/Put it all together.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Put it all together.xcplaygroundpage/Contents.swift
@@ -22,6 +22,33 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 // Replace this comment and add your code below...
+// Draw Background
+canvas.drawRectangle(centreX: 150, centreY: 150, width: 300, height: 300)
+
+// Draw a Border
+canvas.lineColor = Color.white
+for x in stride(from: 0, through: 300, by: 10){
+    canvas.drawLine(fromX: 300, fromY: x, toX: x, toY: 0)
+    canvas.drawLine(fromX: x, fromY: 300, toX: 0, toY: x)
+}
+// If...Else, Draw L or W
+canvas.drawShapesWithBorders = false
+let number = random(from: 0, toButNotIncluding: 2)
+if number == 1{
+    canvas.fillColor = Color.red
+    canvas.drawRectangle(centreX: 100, centreY: 150, width: 20, height: 100)
+    canvas.drawRectangle(centreX: 140, centreY: 100, width: 100, height: 20)
+    canvas.textColor = Color.red
+    canvas.drawText(message: "Take the L", size: 25, x: 80, y: 50)
+} else {
+    canvas.fillColor = Color.blue
+    canvas.drawRectangle(centreX: 100, centreY: 150, width: 20, height: 100)
+    canvas.drawRectangle(centreX: 150, centreY: 140, width: 20, height: 80)
+    canvas.drawRectangle(centreX: 200, centreY: 150, width: 20, height: 100)
+    canvas.drawRectangle(centreX: 150, centreY: 100, width: 120, height: 20)
+    canvas.textColor = Color.blue
+    canvas.drawText(message: "Took the W", size: 25, x: 80, y: 50)
+}
 
 
 /*:
