bdccb704e207cf7ae9c5ed45a74b4ec648bedff9
Eric Xia <exia@lcs.on.ca>
2017-11-24 20:30:38 -0500

    Finished Sequence Example

--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -24,7 +24,25 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 // Replace this comment and add your code below...
-canvas.
+
+// Draw the base and Pole of the Traffic light
+canvas.fillColor = Color.black
+canvas.drawRectangle(centreX: 150, centreY: 150, width: 75, height: 175)
+canvas.drawRectangle(centreX: 150, centreY: 50, width: 15, height: 100)
+
+// Draw Red Light
+canvas.fillColor = Color.red
+canvas.drawEllipse(centreX: 150, centreY: 210, width: 50, height: 50)
+
+// Draw Yellow Light
+canvas.fillColor = Color.yellow
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 50, height: 50)
+
+//Draw Green Light
+canvas.fillColor = Color.green
+canvas.drawEllipse(centreX: 150, centreY: 90, width: 50, height: 50)
+
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
