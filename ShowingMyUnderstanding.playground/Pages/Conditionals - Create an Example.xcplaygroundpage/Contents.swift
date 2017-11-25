cf8f0cdee1eb391784163e695bef7947db1a117e
Eric Xia <exia@lcs.on.ca>
2017-11-24 20:42:40 -0500

    Completed Conditionals Practice

--- ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -20,7 +20,14 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 // Replace this command and add your code below...
-
+let number = random(from: 0, toButNotIncluding: 2)
+if number == 0{
+    canvas.textColor = Color.red
+    canvas.drawText(message: "Roses are red")
+} else{
+    canvas.textColor = Color.blue
+    canvas.drawText(message: "Violets are blue")
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
